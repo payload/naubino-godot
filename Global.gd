395fc12d18cab4ea537264a5f2d8488d27c3af63
Gilbert Röhrbein <payload.git@mailbox.org>
2022-04-09 17:38:45 +0200

    refactor naub naub contact

--- Global.gd
+++ Global.gd
@@ -6,18 +6,27 @@ signal naub_enter_tree(naub)
 
 var NaubLinkScene = preload("res://NaubLink.tscn")
 
+
 func _ready():
 	connect("naub_naub_contact", self, "on_naub_naub_contact")
 
+
 func on_naub_naub_contact(active_naub: Naub, other_naub: Naub):
-	if are_neighbors(active_naub, other_naub): return
-	if not (active_naub.modulate == other_naub.modulate or active_naub.no_links() or other_naub.no_links()): return
-	
+	if active_naub.no_links() or other_naub.no_links():
+		link_two_naubs_together(active_naub, other_naub)
+	elif active_naub.modulate == other_naub.modulate and not are_neighbors(active_naub, other_naub):
+		link_two_naubs_together(active_naub, other_naub)
+		find_and_pop_some_cycle(active_naub, other_naub)
+
+
+func link_two_naubs_together(active_naub: Naub, other_naub: Naub):
 	active_naub.linked_naubs.append(other_naub)
 	other_naub.linked_naubs.append(active_naub)	
 	var link: NaubLink = NaubLinkScene.instance()
 	link.attach_to_naubs(active_naub, other_naub)
-	
+
+
+func find_and_pop_some_cycle(active_naub: Naub, other_naub: Naub):
 	var cycle = find_cycle(active_naub, other_naub)
 	if cycle:
 		for c in cycle:
@@ -28,11 +37,13 @@ func on_naub_naub_contact(active_naub: Naub, other_naub: Naub):
 				c.modulate = Color.green
 			c.pop_away()
 
+
 func are_neighbors(naub_a, naub_b):
 	for naub_x in naub_a.linked_naubs:
 		if naub_x == naub_b or naub_x.linked_naubs.has(naub_b):
 			return true
 	return false
+
 
 func find_cycle(a: Naub, b: Naub):
 	var parents = { a: null }
