e835badc411bec5e64dd0e270aa25d167af0d699
Gilbert Röhrbein <payload.git@mailbox.org>
2022-04-23 22:39:45 +0200

    update page with v0.2

--- NaubGravity.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-func _physics_process(delta):
-	for node in get_tree().get_nodes_in_group("Naub"):
-		var naub: Naub = node
-		var pos = naub.global_position
-		# naub.apply_central_impulse(-0.01 * pos * pos * delta)
--- README.md
+++
@@ -1,3 +0,0 @@
-# naubino
-
-WIP
